963ef324a683c2ec44db1ede4eff8521e99b45d4
Manny Dador <EADador@gmail.com>
2022-04-25 16:59:42 -0400

    Returns count by sentence atm

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,6 @@
+from pydoc import Doc
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -7,7 +10,38 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    with open(file, 'r') as f:
+        file_string = f.read()
+        file_list = file_string.splitlines()
+    for line in file_list:   
+        lower_case_list = line.lower()
+
+        import string
+
+        no_punct = lower_case_list.translate(str.maketrans('', '', string.punctuation))
+
+        a = no_punct.split()
+
+        stop_filtered = [word for word in a if word not in STOP_WORDS]
+        result = ' '.join(stop_filtered)
+        
+        filtered = result.split()
+
+        # List word with count with # and *
+        from collections import Counter
+
+        word_count = Counter(filtered)
+        # doc = [result]
+
+        # count = dict(Counter(word for sentence in doc for word in sentence.split())) # is counting per sentence
+        #count = dict(Counter(word for word in doc))
+        #print(count)
+
+        print(word_count.most_common())
+
+
+
+
 
 
 if __name__ == "__main__":
